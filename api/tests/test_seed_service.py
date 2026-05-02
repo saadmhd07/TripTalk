06ae64bb48ab53a42f3c7922cd2cb4b2bc621cc0
Saad M'HAMDI <96233790+saadmhd07@users.noreply.github.com>
2026-05-02 12:21:56 +0200

    archive docs

--- api/tests/test_seed_service.py
+++ api/tests/test_seed_service.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import StaticPool
 
 from app.core.database import Base
